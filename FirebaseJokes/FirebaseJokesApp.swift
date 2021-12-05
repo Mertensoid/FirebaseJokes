ac8d48e6b5c8565d13646c0bf670120c07b262fa
Mertensoid <matsoukm@yandex.ru>
2021-12-05 10:36:16 +0300

    Initial Commit

---
+++ FirebaseJokes/FirebaseJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseJokesApp.swift
+//  FirebaseJokes
+//
+//  Created by admin on 05.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
